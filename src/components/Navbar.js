b857b2ad68d1901df123ad251c6e228c4afc050c
carolhuy <carolhuynh196@gmail.com>
2023-05-28 15:43:00 -0500

    Font Implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,35 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import './Navbar.css'
+// import './index.css'
+// import './App.css';
+
 function Navbar() {
   return (
     <>
-<nav>
-<Link to='/'>    
-    home testing
-    </Link>
+    <nav className='navStyle' alt="Navigation Bar">
+        <Link to='/' alt="Link to Homepage">    
+          <h1 className='column'>home</h1>
+        </Link>
 
-    <Link to='/about'>    
-    about
-    </Link>
+        <Link to='/about' alt="Link to About Page">    
+          <h1 className='column'>about</h1>
+        </Link>
 
-    <Link to='/research'>    
-    research
-    </Link>
+        <Link to='/research' alt="Link to Research Page">    
+          <h1 className='column'>research</h1>
+        </Link>
 
-    <Link to='/writing'>    
-    writing
-    </Link>
+        <Link to='/writing' alt="Link to Writing Page">    
+          <h1 className='column'>writing</h1>
+        </Link>
 
-    <Link to='/contact'>    
-    contact
-    </Link>
-</nav>
+        <Link to='/contact' alt="Link to Contact Page">    
+          <h1 className='column'>contact</h1>
+        </Link>
+    </nav>
+    <hr/>
     </>
   )
 }
